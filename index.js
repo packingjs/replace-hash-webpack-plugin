ea9f2d2c803145257637619210fc317ac2147ccb
zhongzhi107 <zhong.zhi@163.com>
2016-07-26 08:31:08 +0800

    用packing-glob代替glob

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var path = require('path');
 var fs = require('fs');
 var util = require('util');
 var url = require('url');
-var glob = require('glob');
 var mkdirp = require('mkdirp');
 var endsWith = require('lodash.endswith');
+var packingGlob = require('packing-glob');
 
 var defaultPatternList = [
   {
@@ -29,43 +29,44 @@ ReplaceHashPlugin.prototype.apply = function (compiler) {
   self.options.cwd = self.options.cwd ? (path.isAbsolute(self.options.cwd) ? self.options.cwd : path.resolve(compiler.options.context, self.options.cwd)) : compiler.options.context;
   self.options.dest = path.isAbsolute(self.options.dest) ? self.options.dest : path.resolve(process.cwd(), self.options.dest);
 
-  glob(self.options.src, self.options, function (err, files) {
-    files.forEach(function(file) {
-      var fullpath = path.join(self.options.cwd, file);
-      fs.readFile(fullpath, 'utf8', function (err, data) {
-        compiler.plugin('done', function (stats) {
-          var publicPath = compiler.options.output.publicPath;
-          var jsChunkFileName = compiler.options.output.filename;
-          var cssChunkFileName;
-          // 找出ExtractTextPlugin插件在plugins中的位置
-          compiler.options.plugins.forEach(function(pluginConfig) {
-            if (pluginConfig.filename) {
-              cssChunkFileName = pluginConfig.filename;
+  var patterns = self.options.src;
+  packingGlob(patterns, self.options).forEach(function(file) {
+    var fullpath = path.join(self.options.cwd, file);
+    fs.readFile(fullpath, 'utf8', function (err, data) {
+      compiler.plugin('done', function (stats) {
+        var publicPath = compiler.options.output.publicPath;
+        var jsChunkFileName = compiler.options.output.filename;
+        var cssChunkFileName;
+        // 找出ExtractTextPlugin插件在plugins中的位置
+        compiler.options.plugins.forEach(function(pluginConfig) {
+          if (pluginConfig.filename) {
+            cssChunkFileName = pluginConfig.filename;
+          }
+        });
+        Object.keys(stats.compilation.assets).forEach(function(item) {
+          var ext = path.extname(item); //.js
+          var name = path.basename(item, ext); //main-e1bb26
+          // 只处理html中的css、js
+          if (['.js', '.css'].indexOf(ext) != -1) {
+            var filename;
+            if (ext === '.js') {
+              filename = jsChunkFileName;
             }
-          });
-          Object.keys(stats.compilation.assets).forEach(function(item) {
-            var ext = path.extname(item); //.js
-            var name = path.basename(item, ext); //main-e1bb26
-            // 只处理html中的css、js
-            if (['.js', '.css'].indexOf(ext) != -1) {
-              var filename;
-              if (ext === '.js') {
-                filename = jsChunkFileName;
-              }
-              if (ext === '.css') {
-                filename = cssChunkFileName;
-              }
-              var hashLengthMatches = filename.match(/\[\S*hash:(\d)\]/i);
-              var hashLength;
-              if (hashLengthMatches[1]) {
-                hashLength = hashLengthMatches[1];
-              }
-              var regString = filename
-                .replace('\[name\]','(\\S+)')
-                .replace('\[chunkhash:' + hashLength + '\]', '\\w{' + hashLength + '}')
-                .replace('\[contenthash:' + hashLength + '\]', '\\w{' + hashLength + '}')
-                .replace('\[hash:' + hashLength + '\]', '\\w{' + hashLength + '}');
-              var matches = item.match(new RegExp(regString));
+            if (ext === '.css') {
+              filename = cssChunkFileName;
+            }
+            var hashLengthMatches = filename.match(/\[\S*hash:(\d)\]/i);
+            var hashLength;
+            if (hashLengthMatches[1]) {
+              hashLength = hashLengthMatches[1];
+            }
+            var regString = filename
+              .replace('\[name\]','(\\S+)')
+              .replace('\[chunkhash:' + hashLength + '\]', '\\w{' + hashLength + '}')
+              .replace('\[contenthash:' + hashLength + '\]', '\\w{' + hashLength + '}')
+              .replace('\[hash:' + hashLength + '\]', '\\w{' + hashLength + '}');
+            var matches = item.match(new RegExp(regString));
+            if (matches) {
               var oldFilename = matches[1] + ext;
               var oldPath = path.join(publicPath, oldFilename); // /assets/main.js
               var newPath = path.join(publicPath, item);
@@ -76,29 +77,32 @@ ReplaceHashPlugin.prototype.apply = function (compiler) {
                 newPath = self.options.assetsDomain + newPath;
               }
               data = self.doReplace(oldPath, newPath, data);
+            } else {
+              console.log(`[warnings]${item} replace hash failed.`);
             }
-          });
 
-          // 将rev处理的文件也替换一遍
-          if (compiler.revSourceMap) {
-            Object.keys(compiler.revSourceMap).forEach(function(item) {
-              var newPath = compiler.revSourceMap[item];
-              if (self.options.assetsDomain) {
-                newPath = url.resolve(self.options.assetsDomain, newPath);
-              }
-              data = self.doReplace(item, newPath, data);
-            });
           }
-
-          var dest = path.resolve(self.options.dest, file);
-          var destDir = path.dirname(dest);
-          if (!fs.existsSync(destDir)) {
-            mkdirp.sync(destDir);
-          }
-          fs.writeFileSync(dest, data);
-          console.log(dest + ' created.');
-
         });
+
+        // 将rev处理的文件也替换一遍
+        if (compiler.revSourceMap) {
+          Object.keys(compiler.revSourceMap).forEach(function(item) {
+            var newPath = compiler.revSourceMap[item];
+            if (self.options.assetsDomain) {
+              newPath = url.resolve(self.options.assetsDomain, newPath);
+            }
+            data = self.doReplace(item, newPath, data);
+          });
+        }
+
+        var dest = path.resolve(self.options.dest, file);
+        var destDir = path.dirname(dest);
+        if (!fs.existsSync(destDir)) {
+          mkdirp.sync(destDir);
+        }
+        fs.writeFileSync(dest, data);
+        console.log(dest + ' created.');
+
       });
     });
   });
